cc36dc58f140dfbc1ec692436cc698eb91abd48d
honnosouta <92144750+honnosouta@users.noreply.github.com>
2022-11-09 10:13:49 +0900

    heart_change

--- public/js/morita_03.js
+++ public/js/morita_03.js
@@ -1,7 +1,12 @@
 const btn = document.querySelector("#like-btn");
-
+let sw = true;
 
 btn.onclick = function(event){
-    btn.style.backgroundImage = "url(img/heart_pink.svg)";
-    // btn.style.backgroundImage = "url(img/heart_pink.svg)";
+    if( sw == true ){
+        btn.style.backgroundImage = "url(img/heart_pink.svg)";
+        sw = false;
+    }else{
+        btn.style.backgroundImage = "url(img/heart_blue.svg)";
+        sw = true;
+    }
 }
